f5007110c684f461622103b97ad388bd92253fea
davismarciel <davisdevi05@gmail.com>
2023-03-10 23:05:03 -0300

    first commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,100 @@
+import React, { Component } from 'react';
+import './Main.css';
+
+import Form from './Form';
+import Tasks from './Tasks';
+
+export default class Main extends Component {
+  state = {
+    newTask: '',
+    tasks: [],
+    index: -1,
+  };
+
+  componentDidMount() {
+    const tasks = JSON.parse(localStorage.getItem('tasks'));
+
+    if (!tasks) return;
+
+    this.setState({ tasks });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { tasks } = this.state;
+
+    if (tasks === prevState.tasks) return;
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { tasks, index } = this.state;
+    let { newTask } = this.state;
+    newTask = newTask.trim();
+
+    const newTasks = [...tasks];
+
+    if (tasks.indexOf(newTask) !== -1 || !newTask) return;
+
+    if (index === -1) {
+      this.setState({
+        tasks: [...newTasks, newTask],
+        newTask: '',
+      });
+    } else {
+      newTasks[index] = newTask;
+      this.setState({
+        tasks: [...newTasks],
+        index: -1,
+        newTask: '',
+      });
+    }
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      newTask: e.target.value,
+    });
+  };
+
+  handleEdit = (e, index) => {
+    const { tasks } = this.state;
+
+    this.setState({
+      index,
+      newTask: tasks[index],
+    });
+  };
+
+  handleDelete = (e, index) => {
+    const { tasks } = this.state;
+    const newtasks = [...tasks];
+
+    newtasks.splice(index, 1);
+
+    this.setState({
+      tasks: [...newtasks],
+    });
+  };
+
+  render() {
+    const { newTask, tasks } = this.state;
+
+    return (
+      <div className="main">
+        <h1>To Do List</h1>
+        <Form
+          handleSubmit={this.handleSubmit}
+          handleChange={this.handleChange}
+          newTask={newTask}
+        />
+        <Tasks
+          tasks={tasks}
+          handleEdit={this.handleEdit}
+          handleDelete={this.handleDelete}
+        />
+      </div>
+    );
+  }
+}
